b9bc04f4e5ff3c4a563e7b784757d423d15af868
Kartal Kaan Akdoğan <kartalkaanakdogan@hotmail.com>
2021-01-07 12:30:36 +0300

    Yeni değişiklik

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba Dünya"); Console.ReadLine();  
+            Console.WriteLine("Merhaba Dünya"); Console.ReadLine();  Console.WriteLine("Merhaba"); 
         }
     }
 }
